6e54927903017e4fc413aecac20b046f964c3742
rladbgus <cocokiuuu1858@gmail.com>
2021-12-07 11:49:52 +0900

    21.12.07 algorithm 3

--- 2021/21.12.07.js
+++ 2021/21.12.07.js
@@ -20,11 +20,21 @@ x만큼 간격이 있는 n개의 숫자
 
 함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다. 다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
  */
-
 function solution(x, n) {
     let answer = [];
     for(let i=1; i<n+1; i++){
         answer.push(x*i);
     }
+    return answer;
+}
+
+/**
+ * 평균 구하기
+
+정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
+ */
+function solution(arr) {
+    let answer = 0;
+    answer = arr.reduce((acc, cur) => acc + cur, 0) / arr.length
     return answer;
 }
